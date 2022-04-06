6f4dec42cbc0b7d1bc05996bd3d4bfd3206c63cf
Paul van Schayck <polleke@gmail.com>
2022-04-06 11:12:42 +0200

    Updates to NPS calculation. Include simulation of gaussian filtered frames.

--- nps/measureNPS.py
+++ nps/measureNPS.py
@@ -5,6 +5,7 @@ import mrcfile
 import numpy as np
 import os
 from scipy import fftpack
+from scipy.ndimage import gaussian_filter
 from scipy.optimize import curve_fit
 from skimage.transform import downscale_local_mean
 
@@ -15,6 +16,7 @@ def parse_arguments():
     parser.add_argument('FILE', nargs='?', help="Input image stack of flat fields (mrcs).")
     parser.add_argument('--super_res', default=1, type=int, help='Rescale the frequency of the measured NPS curve by this factor')
     parser.add_argument('--store', type=str, help='Store output measured MTF curve')
+    parser.add_argument('--crop', default=0, type=int, help='Crop the image to this (power of 2) size. This helps with NPS(0) estimates.')
 
     settings = parser.parse_args()
 
@@ -22,8 +24,8 @@ def parse_arguments():
 
 
 # A logistic function used for fitting NPS(0)
-def nps0_fit(x, a, b, c):
-    return a*(x+b)/(x+c)
+def nps0_fit(x, a, b):
+    return a*x/(x+b)
 
 
 def power_spectrum(d):
@@ -56,20 +58,54 @@ def calculate_nnps(nps, nps0):
     return np.divide(nps, nps0)
 
 
+# TODO: This fourier cropping is not working! It may be a better alternative to downscale_local_mean()
+def fourier_crop_bin(f, factor):
+    # Take the fourier transform of the image.
+    f1 = fftpack.fft2(f)
+
+    # Now shift the quadrants around so that low spatial frequencies are in
+    # the center of the 2D fourier transformed image.
+    f2 = fftpack.fftshift(f1)
+
+    # Calculate center
+    center = tuple(int(s / 2) for s in f2.shape)
+
+    # Calculate crop
+    s = tuple(int(s / (factor*2)) for s in f2.shape)
+
+    # Perform fourier cropping by taking every 2nd pixel
+    crop = f1[center[0]-1-s[0]:center[0]-1+s[0], center[1]-1-s[1]:center[1]-1+s[1]]
+
+    # Return inverse transformed image
+    i = np.abs(fftpack.ifft2(fftpack.ifftshift(crop)))
+
+    return i
+
+
 def calculate_nps0(frames, mean):
     r = list()
+
+    # Calculate the bin factors to measure. Do this as power of 2, to
+    factors = 2**np.arange(1, np.log2(mean.shape[0]), dtype=int)
+
+    # Limit the bin factors to measure to not too small image sizes
+    factors = factors[0:-3]
+
     for frame in frames:
+        # Subtract frame from the mean
         f = frame - mean
-        for factor in range(1, 50, 1):
+
+        for factor in factors:
             b = 1/factor
 
             # Bin the frame using the mean of the surrounding pixels
-            binned = downscale_local_mean(f, (factor, factor))
+            binned = downscale_local_mean(f, (factor, factor), cval=np.mean(f))
 
             # Calculate the variance
             sigma_squared = np.var(binned)
 
             # This does NOT correspond to the sigma^2/b^2 as written in McMullan et al. 2009 and Paton et al. 2021!
+            # But it does match the formula in Shaw's Image Science
             nps0 = sigma_squared*factor**2
 
             # print("shape: %s, factor: %d, b: %.2f, var: %s, nps0: %.5f" % (binned.shape, factor, b, sigma_squared, nps0))
@@ -83,7 +119,10 @@ def calculate_nps0(frames, mean):
 config = parse_arguments()
 
 if config.FILE is None:
-    frames = np.random.normal(256, 16, (10, 512, 512))
+    frames = np.random.normal(256, 16, (100, 512, 512))
+
+    # Apply gaussian filter to simulated data
+    frames = gaussian_filter(frames, sigma=(0, 0.5, 0.5))
 
     # Calculate the mean of all pixels
     mean = np.mean(frames, axis=0)
@@ -91,9 +130,19 @@ if config.FILE is None:
     config.FILE = 'Simulated'
 else:
     with mrcfile.open(config.FILE, mode='r') as f:
-        frames = f.data
+        frames = list()
+
+        # Crop the frames as needed
+        for fr in f.data:
+            if config.crop > 0:
+                frames.append(fr[0:config.crop, 0:config.crop])
+            else:
+                frames.append(fr)
+
+        frames = np.array(frames)
+
         # Calculate the mean of all pixels
-        mean = np.mean(f.data, axis=0)
+        mean = np.mean(frames, axis=0)
 
 ps = list()
 for frame in frames:
@@ -103,23 +152,28 @@ for frame in frames:
 # Calculate the 2D NPS by taking the average of all individual NPS, and dividing by the number of pixels
 # Paton 2021 et al. (eq 2)
 nps = np.mean(np.array(ps), axis=0)/(mean.shape[0]*mean.shape[1])
+nps_1d = radial_profile(nps)
 
 # Calculate NPS(0) as function of the binning factor
 nps0_meas = calculate_nps0(frames, mean)
 
 # Make an initial guess for the fitting, by taking the first 10% of the data as NPS(0)
-nps0_g = np.mean(radial_profile(nps)[0:10])
+nps0_g = np.mean(nps_1d[0:int(len(nps_1d)*0.1)])
 print("Guessed NPS(0): %0.2f" % nps0_g)
-fit_guess = [nps0_g, 1, 1]
-fit_bounds = ([nps0_g - nps0_g*0.1, 0, 0], [nps0_g + nps0_g*0.1, np.inf, np.inf])
+fit_guess = [nps0_g, 1]
+fit_bounds = ([nps0_g - nps0_g*0.2, 0], [nps0_g + nps0_g*0.2, np.inf])
 
 # Fit
 fit, pcov = curve_fit(nps0_fit, nps0_meas[:, 0], nps0_meas[:, 1], maxfev=100000, p0=fit_guess, bounds=fit_bounds)
-x_fit = np.arange(0, 50)
-print("Fitted NPS(0): %0.2f" % fit[0])
+nps0_f = fit[0]
+x_fit = np.arange(0, np.max(nps0_meas[:, 0])+1)
+print("Fitted NPS(0): %0.2f" % nps0_f)
 
-# Normalize the NPS using the fitted NPS(0)
-nnps = calculate_nnps(nps, fit[0])
+# Used guessed NPS(0)
+nps0 = nps0_g
+
+# Normalize the NPS using the selected NPS(0)
+nnps = calculate_nnps(nps, nps0)
 
 # Take the radial profile to create a 1D NPS
 nnps_1d = radial_profile(nnps)
@@ -152,13 +206,14 @@ im = ax2.imshow(nps)
 fig.colorbar(im, ax=ax2, orientation='vertical')
 ax2.set_title("Noise Power Spectrum (NPSdig)")
 
-# Calculating NPS0
+# Calculating NPS(0)
 ax3.scatter(nps0_meas[:, 0], nps0_meas[:, 1], label='Measured')
-ax3.hlines(y=fit[0], xmin=0, xmax=np.max(nps0_meas[:, 0]), color='r', label='nps0')
-ax3.plot(x_fit, nps0_fit(x_fit, *fit), color='orange', label='%.02f*(x+%0.2f)/(x+%0.2f)' % (fit[0], fit[1], fit[2]))
+ax3.hlines(y=nps0_g, xmin=0, xmax=np.max(nps0_meas[:, 0]), color='r', label='NPS(0) (10%)')
+ax3.hlines(y=nps0_f, xmin=0, xmax=np.max(nps0_meas[:, 0]), color='orange', label='NPS(0) (fitted)')
+ax3.plot(x_fit, nps0_fit(x_fit, *fit), color='orange', label='%.02f*x/(x+%0.2f)' % (fit[0], fit[1]))
 ax3.set_ylim(0)
 ax3.set_xlabel("Factor")
-ax3.set_ylabel("nps0")
+ax3.set_ylabel("NPS(0)")
 ax3.legend(loc='lower right')
 ax3.set_title("Estimating NPS(0)")
 
